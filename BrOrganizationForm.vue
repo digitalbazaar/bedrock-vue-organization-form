e2993fc3c550e5d51ff08210f752a9d0250e4808
Ganesh Annan <gannan@digitalbazaar.com>
2019-02-12 11:20:12 -0500

    Complete initial form component.

--- BrOrganizationForm.vue
+++ BrOrganizationForm.vue
@@ -1,5 +1,39 @@
 <template>
-  <div>
+  <div class="fit">
+    <div class="q-mt-md">
+      <q-field :error="$v.legalName.$error">
+        <q-input
+          v-model="legalName.value"
+          :float-label="legalName.label"
+          class="q-pa-sm q-mt-md"
+          @blur="$v.legalName.$touch"
+          @keyup="$v.legalName.$touch" />
+      </q-field>
+    </div>
+    <!-- <div class="col-xs-12">
+      <q-field :error="$v.logo.$error">
+        <q-select
+          v-model="logo.value"
+          :float-label="logo.label"
+          filter
+          :options="entityLogos"
+          class="q-pa-sm q-mt-md"
+          @blur="$v.logo.$touch"
+          @keyup="$v.logo.$touch" />
+      </q-field>
+    </div> -->
+    <div class="col-xs-12">
+      <q-field :error="$v.entityType.$error">
+        <q-select
+          v-model="entityType.value"
+          :float-label="entityType.label"
+          filter
+          :options="entityTypes"
+          class="q-pa-sm q-mt-md"
+          @blur="$v.entityType.$touch"
+          @keyup="$v.entityType.$touch" />
+      </q-field>
+    </div>
   </div>
 </template>
 
@@ -9,9 +43,88 @@
  */
 'use strict';
 
+import {minLength, required} from 'vuelidate/lib/validators';
+import entityTypes from './entityTypes';
+
 export default {
-  name: 'BrOrganizationForm'
+  name: 'BrOrganizationForm',
+  props: {
+    value: {
+      type: Object,
+      required: true,
+      default: () => ({})
+    }
+  },
+  validations: {
+    legalName: {
+      value: {
+        minLength: minLength(1),
+        required
+      }
+    },
+    logo: {
+      value: {
+        minLength: minLength(1)
+      }
+    },
+    entityType: {
+      value: {
+        minLength: minLength(1),
+        required
+      }
+    }
+  },
+  data() {
+    return {
+      entityTypes
+    };
+  },
+  computed: {
+    legalName() {
+      return this.value.legalName;
+    },
+    logo() {
+      return this.value.logo;
+    },
+    entityType() {
+      return this.value.entityType;
+    },
+    valid() {
+      return !this.$v.$invalid;
+    }
+  },
+  created() {
+    this.setup();
+  },
+  methods: {
+    setup() {
+      const updatedLabels = _applyDefaultLabels({
+        data: this.value,
+        labels: {
+          legalName: 'Legal Name',
+          logo: 'Entity Logo',
+          entityType: 'Entity Type'
+        }});
+      this.$emit('input', updatedLabels);
+    }
+  }
 };
+
+// TODO: Move to bedrock-web-forms
+function isString(str) {
+  return str && typeof str === 'string';
+}
+
+function _applyDefaultLabels({data, labels}) {
+  return Object.keys(data).reduce((acc, key) => {
+    acc[key] = isString(data[key].label) ? data[key] :
+      {
+        ...data[key],
+        label: labels[key]
+      };
+    return acc;
+  }, {});
+}
 
 </script>
 <style>
---
+++ entityTypes.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    "label": "General Partnership",
+    "value": "GP"
+  },
+  {
+    "label": "Limited Partnership",
+    "value": "LP"
+  },
+  {
+    "label": "Limited Liability Partnership",
+    "value": "LLP"
+  },
+  {
+    "label": "Limited Liability Limited Company",
+    "value": "LLLP"
+  },
+  {
+    "label": "Limited Liability Company",
+    "value": "LLC"
+  },
+  {
+    "label": "C-Corporation",
+    "value": "C-CORP"
+  },
+  {
+    "label": "S-Corporation",
+    "value": "S-CORP"
+  },
+  {
+    "label": "Professional Corporation",
+    "value": "PC"
+  }
+];
